04ed4d5598640147d0f08de24e66a9c04cc1a12a
thaiminhpv <thaiminhpv@gmail.com>
2020-02-13 11:21:31 +0700

    packed -> distributed

--- min.ino
+++ min.ino
@@ -1,3 +1,4 @@
+#include <ArduinoJson.h>
 //#include <TimeLib.h>
 #include <WiFiClientSecure.h> 
 #include <SoftwareSerial.h>
@@ -32,14 +33,16 @@ OLEDDisplayUi ui     ( &display );
 //--------------------
 //constant
 #define FRAME_RATE 200L
-//#define D5 (14)
-//#define D6 (12)
 #define BAUD_RATE 57600
 
-const char *penID = "pen1";
 
-const char *ssid = "FreeWifi";
-const char *password = "87654231";
+//web settings
+const char *penID = "pen1";
+// const char *ssid = "FreeWifi";
+// const char *password = "87654231";
+
+const char *ssid = "Hackathon 2019";
+const char *password = "Hackathon@2019";
 //TODO: list wifi
 //https://watertestinginstant.firebaseapp.com/new-noti
 const char *host = "watertestinginstant.firebaseapp.com";
@@ -47,30 +50,16 @@ const String subdirectory = "/new-noti";
 const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
 const uint8_t fingerprint[20] = {0x46, 0xf2, 0xe8, 0x99, 0x89, 0x6d, 0x93, 0xc2, 0x44, 0xe0, 0x44, 0x22, 0xd0, 0x86, 0x9b, 0xf2, 0x56, 0xa7, 0x7c, 0x95};
 
+//location
+const char *hostGoogle = "www.googleapis.com";
+const String API_KEY_GOOGLE = "/geolocation/v1/geolocate?key=AIzaSyDfg_X_OquHuYwe6iTsQlUC6y7DPNkVrXc";
+//https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDfg_X_OquHuYwe6iTsQlUC6y7DPNkVrXc
+
+
+
 //I/O
-#define TURBIDITY_SENSOR 0
+#define TURBIDITY_SENSOR 12
 #define PH_SENSOR 13
-// #if (ESP8266)
-//   #define BUTTON D3 //D3 D6! D7! ---- gạt sang bên trái để bật, sang phải để reset
-// #elif (ESP32)
-//   #define BUTTON 18
-// #endif
-// #ifdef LED_BUILTIN
-//   #define LED_NOTIFY LED_BUILTIN
-// #else
-//   #define LED_NOTIFY 5
-// #endif
-
-//bluetooth config
-//#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-//  bool isBluetoothEnabled = false;
-//#else
-//  bool isBluetoothEnabled = true;
-//#endif
-
-//constants
-// #define MEASURING_SCREEN 0
-// #define RESULT_SCREEN 1
 
 #define SCANNING 0
 #define DISCONNECTED 1
@@ -80,8 +69,6 @@ const uint8_t fingerprint[20] = {0x46, 0xf2, 0xe8, 0x99, 0x89, 0x6d, 0x93, 0xc2,
 
 //variables
 int wifi_status = DISCONNECTED;
-// int bluetooth_status = DISCONNECTED;
-// int frameController = MEASURING_SCREEN;
 bool isMeasuring = true;
 
 //frame tracking variables
@@ -98,14 +85,9 @@ int last_Turbidity = 0;
 // BlynkTimer buttonHandler;
 BlynkTimer wifiScanner;
 // BlynkTimer bluetoothHandler;
-BlynkTimer dataSender;
+// BlynkTimer dataSender;
 
 SoftwareSerial TdsSensor(14, 23);
-
-//#if (ESP32)
-//BluetoothSerial SerialBT;
-//#endif
-//----------------------------------
 
 void setup() {
   pinMode(16, OUTPUT);
@@ -116,48 +98,20 @@ void setup() {
   Serial.begin(BAUD_RATE);
   Serial.println("Starting...");
 
-  //sensors
-  // pinMode(LED_NOTIFY, OUTPUT);
-  // digitalWrite(LED_NOTIFY, LOW);
-  // pinMode(BUTTON, INPUT);
-
   TdsSensor.begin(9600);
   TdsSensor.printf("AT+MODE=1\r\n");
 
 
   // buttonHandler.setInterval(200, handleButtonClick);
   wifiScanner.setInterval(1000, scanWifi);
-  dataSender.setInterval(10000, sendDataToExternal);
+  // dataSender.setInterval(10000, sendDataToExternal);
 
-  //****Bluetooth****
-//  #if (ESP32)
-//    SerialBT.begin("Water Testing Instant");//-----------------------------------------------------------------------here
-//    if (!isBluetoothEnabled
-//        || !btStart()
-//        || esp_bluedroid_init() != ESP_OK
-//        || esp_bluedroid_enable() != ESP_OK
-//    ) //Bluetooth available?
-//    {
-//      //Your chip doesn't support bluetooth
-//      bluetooth_status = DISCONNECTED;
-//    } else {
-//      printDeviceAddress();
-//      bluetoothHandler.setInterval(200L, bluetoothHandle);
-//      bluetooth_status = SCANNING;
-//    }
-//  #endif
-//  //***
-  
   ui.init();
   // display.flipScreenVertically();
 }
 
 void loop() {
-  wifiScanner.run();
-  Serial.println(analogRead(TURBIDITY_SENSOR));
-  // buttonHandler.run();
   frameUpdate();
-  // delay(FRAME_RATE);
 }
 //-------
 void frameUpdate () {
@@ -170,42 +124,17 @@ void frameUpdate () {
   }
   display.display();
 }
-//
-
-// int buttonStatus = HIGH; //invert
-// void handleButtonClick () {
-//   buttonStatus = digitalRead(BUTTON);
-//   if (buttonStatus == LOW) {
-//     if (frameController == MEASURING_SCREEN) {
-//       frameController = RESULT_SCREEN;
-//       measuringCount = resultCount = 0;
-//     } else {
-//       frameController = MEASURING_SCREEN;
-//       measuringCount = resultCount = 0;
-//     }
-//   }
-// }
-
 
 //--------------Wifi Manager-----------------
 int status = WL_IDLE_STATUS;
 void scanWifi() {
   if (status == WL_IDLE_STATUS && wifi_status == DISCONNECTED) {
-    // int numAvailableNetworks = WiFi.scanNetworks();
-    // if (numAvailableNetworks < 0) { 
-    //   Serial.println("Couldn't get a wifi connection");
-    //   wifi_status = SCANNING;
-    // } else {
-    //   for (int thisWifi = 0; thisWifi < numAvailableNetworks; thisWifi++) {
-    //     Serial.println(WiFi.SSID(thisWifi));
-    //   }
       Serial.println("-------------------\nAttempt connecting to network: ");
       Serial.println(WiFi.SSID());
       WiFi.mode(WIFI_OFF);
       WiFi.mode(WIFI_STA);
       WiFi.begin(ssid, password);
       wifi_status = CONNECTING;
-    // }
   } else if (WiFi.status() == WL_CONNECTED && wifi_status != CONNECTED) {
     wifi_status = CONNECTED;
     status = WL_CONNECTED;
@@ -219,14 +148,53 @@ void scanWifi() {
 bool sendDataToCloudViaWifi() {
   if (WiFi.status() == WL_CONNECTED) { //double check
     Serial.println("Data sending...");
-    WiFiClientSecure httpsClient;
     #if (ESP8266)
       httpsClient.setFingerprint(fingerprint);
     // #elif (ESP32)
     //   httpsClient.setCACert(certificate);
     #endif
-    httpsClient.setTimeout(15000);
+
+    //get location
+    Serial.println("Get location...");
+    WiFiClientSecure httpsClientLocation;
+    httpsClientLocation.setTimeout(15000);
     int r=0; //retry counter
+    while((!httpsClientLocation.connect(hostGoogle, httpsPort)) && (r < 30)){
+        // delay(100);
+        Serial.print(".");
+        r++;
+    }
+    if(r==30) {
+      Serial.println("Get location failed");
+    } else {
+      Serial.println("Get location!");
+    }
+    httpsClientLocation.print("POST " + API_KEY_GOOGLE + " HTTP/1.1\r\n" +
+                "Host: " + hostGoogle + "\r\n" +
+                "Content-Type: application/json"+ "\r\n" +
+                "Content-Length: " + "0" + "\r\n\r\n" +
+                "\r\n" +
+                "Connection: close\r\n\r\n");
+
+    while (httpsClientLocation.connected()) {
+      String line = httpsClientLocation.readStringUntil('\n');
+      if (line == "\r") {
+        Serial.println("Location received");
+        break;
+      }
+    }
+
+    Serial.println("location is: ");
+    String location;
+    while(httpsClientLocation.available()){        
+      location += httpsClientLocation.readStringUntil('\n');
+    }
+    location = location.substring(0, location.lastIndexOf("HTTP") - 1);
+
+    //submit data to server
+    WiFiClientSecure httpsClient;
+    httpsClient.setTimeout(15000);
+    r=0; //retry counter
     while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
         // delay(100);
         Serial.print(".");
@@ -237,8 +205,10 @@ bool sendDataToCloudViaWifi() {
     } else {
       Serial.println("Connected to web");
     }
+
     // String data = "{\"ph\":" + String(last_pH) + ",\"tds\":" + String(last_TDS) + ",\"turbidity\":" + String(last_Turbidity) + "}";
-    String data = String(last_pH) + "-" + String(last_TDS) + "-" + String(last_Turbidity) + "-" + String(penID);
+    String data = String(last_pH) + "_" + String(last_TDS) + "_" + String(last_Turbidity) + 
+                  "_" + String(penID) + "_" + location;
     
     httpsClient.print("POST " + subdirectory + " HTTP/1.1\r\n" +
                 "Host: " + host + "\r\n" +
@@ -246,7 +216,7 @@ bool sendDataToCloudViaWifi() {
                 "Content-Length: " + String(data.length()) + "\r\n\r\n" +
                 data + "\r\n" +
                 "Connection: close\r\n\r\n");
-    
+
     if (httpsClient.connected()) {
       Serial.println(data);
       Serial.println("data sent via wifi");
@@ -259,59 +229,33 @@ bool sendDataToCloudViaWifi() {
     return false;
   }
 }
-//------------Bluetooth Manager------------------
-//#if (ESP32)
-//  void bluetoothHandle () {
-//    if (Serial.available()) {
-//          SerialBT.write(Serial.read());
-//    }
-//    if (SerialBT.available()) {
-//      bluetooth_status = CONNECTED;
-//      Serial.println(SerialBT.read());
-//    }
-//  }
-//  void printDeviceAddress() {
-//    const uint8_t* point = esp_bt_dev_get_address();
-//    for (int i = 0; i < 6; i++) {
-//      char str[3];
-//      sprintf(str, "%02X", (int)point[i]);
-//      Serial.print(str);
-//      if (i < 5){
-//        Serial.print(":");
-//      }
-//    }
-//  }
-//  bool sendDataViaBluetooth() {
-//    String data = "{\"ph\":" + String(last_pH) + ",\"tds\":" + String(last_TDS) + ",\"turbidity\":" + String(last_Turbidity) + "}";
-//    return false;
-//  }
-//#endif
-
 //--------------------View--------------------
 void measuringScreen() {
   UIOverlay();
   readSensor();
   displayData();
   measuringCount++;
-  if (measuringCount > 4000) {
+  if (measuringCount > 750) {
     measuringCount = resultCount = 0;
     isMeasuring = false;
   }
 }
 void resultScreen() {
-  if (resultCount % 700 > 400) {
+  if (resultCount % 1000 > 650) {
     UIOverlay();
     displayData();
+
+    wifiScanner.run();
+    if (!isDataSent) {
+      sendDataToExternal();
+    }
   }
   resultCount++;
   if (resultCount > 3500) {
     measuringCount = resultCount = 0;
     // frameController = MEASURING_SCREEN;
   }
-  if (!isDataSent) {
-    dataSender.run();
-    isDataSent = true;
-  }
+  
 };
 
 //---
@@ -331,15 +275,15 @@ void displayData() {
   //pH
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_24);
-  display.drawString(0 , 25, String(last_pH));
+  display.drawString(0 , 10, last_pH == 14 ? "N/A" : String(last_pH));
   //TDS
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setFont(ArialMT_Plain_24);
-  display.drawString(60 , 25, String(last_TDS));
+  display.drawString(60 , 10, String(last_TDS));
   //Turbidity
   display.setTextAlignment(TEXT_ALIGN_RIGHT);
   display.setFont(ArialMT_Plain_24);
-  display.drawString(125 , 25, String(last_Turbidity));
+  display.drawString(125 , 10, String(last_Turbidity));
 }
 
 
@@ -353,7 +297,7 @@ void readSensor() {
   TdsSensor.printf("AT+VALUE=?\r\n");
   String tds_measured = TdsSensor.readStringUntil('\n');
   //nếu nhúng vào nước
-  if (tds_measured.toInt() != 0) {
+  if (tds_measured.toInt() != 0) {/////---------------------------------------------------
     // last_TDS = tds_measured.toInt();
     // //Turbidity
     // last_Turbidity = 1000 - (analogRead(TURBIDITY_SENSOR) * 1000 / 4095);
@@ -381,7 +325,7 @@ void readSensor() {
     last_pH = total_pH / 30 ;
     last_TDS = total_TDS / 30;
     last_Turbidity = total_Turbidity / 30;
-    Serial.println(last_Turbidity);
+    // Serial.println(last_Turbidity);
 
     total_pH = total_TDS = total_Turbidity = 0;
     sampleCount = 0;
@@ -389,12 +333,7 @@ void readSensor() {
 }
 
 void sendDataToExternal() {
-  if(!sendDataToCloudViaWifi()) {
-//    #if (ESP32)
-//      if(sendDataViaBluetooth) {
-//        Serial.println("Data sent via bluetooth");
-//      } else
-//    #endif
+  if(!(isDataSent = sendDataToCloudViaWifi())) {
       Serial.println("Data can't be sent");
   }
 }
